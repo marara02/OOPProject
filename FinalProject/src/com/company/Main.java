6299ad9f41f0b29ae7016d034cadde2675304fe0
marara02 <57908460+marara02@users.noreply.github.com>
2020-06-09 14:49:38 +0600

    Update Main.java

--- FinalProject/src/com/company/Main.java
+++ FinalProject/src/com/company/Main.java
@@ -2,44 +2,5 @@ package com.company;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-        System.out.print("-----------------------------------WELCOME TO ONLINE BOOK SHOP------------------------------------------");
-
-        FashionArticles fashionArticles = new FashionArticles("2020 April");
-        ScientificArticles scientificArticles = new ScientificArticles("NASA","2020 April");
-        EducationalBooks educationalBooks = new EducationalBooks();
-        LiteratureBooks literatureBooks = new LiteratureBooks();
-
-        DatabaseBook book = DatabaseBook.getInstance();
-        book.query("SELECT * FROM books");
-
-        System.out.println("Choose what type you want to purchase");
-        System.out.println("1.Book");
-        System.out.println("2.Article");
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        switch (n){
-            case 1:
-                String type = scanner.nextLine();
-                if(type.equals("Educational")){
-                   educationalBooks.action();
-                }
-                if(type.equals("Literature")){
-                    literatureBooks.action();
-                }
-                break;
-            case 2:
-                System.out.println("Choose liked article");
-                String article = scanner.nextLine();
-                if(article.equals("Scientific")){
-                    scientificArticles.action();
-                }
-                if(article.equals("Fashion")){
-                    fashionArticles.action();
-                }
-                break;
-            default:
-                return;
-        }
     }
 }
