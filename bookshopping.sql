1bd4ed5d534840d7ca3677d1431b7eae70d21cbf
marara02 <57908460+marara02@users.noreply.github.com>
2020-06-12 02:53:52 +0600

    Totally all database

--- bookshopping.sql
+++ bookshopping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Июн 11 2020 г., 15:57
+-- Время создания: Июн 11 2020 г., 20:52
 -- Версия сервера: 8.0.18
 -- Версия PHP: 7.3.16
 
@@ -48,6 +48,48 @@ INSERT INTO `articles` (`id`, `article_name`, `genre`, `published_at`, `cost`) V
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `Full_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_1` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_2` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_3` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_4` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_5` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `educational`
+--
+
+CREATE TABLE `educational` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `grade` int(11) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `author` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `educational`
+--
+
+INSERT INTO `educational` (`id`, `subject`, `grade`, `cost`, `author`) VALUES
+(1, 'math', 11, 3240, 'Atamura'),
+(2, 'Math', 10, 3240, 'Atamura'),
+(3, 'Biology', 7, 1700, 'Atamura'),
+(4, 'russian language', 1, 2300, 'Atamura'),
+(5, 'English', 1, 2500, 'Cambridge OFC');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `literature`
 --
 
@@ -67,7 +109,9 @@ INSERT INTO `literature` (`id`, `book_name`, `author`, `genre`, `cost`) VALUES
 (1, 'The last Camelia', 'Sarah Jio', 'roman', 1200),
 (2, 'Inferno', 'Dan Brown', 'drama', 2000),
 (3, 'Pride and prejudice', 'Jane Austin', 'Lovely Roman', 990),
-(4, 'The Da Vinci Code', 'Dan Brown', 'Detective', 1780);
+(4, 'The Da Vinci Code', 'Dan Brown', 'Detective', 1780),
+(5, 'Emma', 'Jane Austin', 'Lovely roman', 1890),
+(6, 'Shantaram', 'gregory david roberts', 'roman,autobiography', 5200);
 
 --
 -- Индексы сохранённых таблиц
@@ -77,6 +121,18 @@ INSERT INTO `literature` (`id`, `book_name`, `author`, `genre`, `cost`) VALUES
 -- Индексы таблицы `articles`
 --
 ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `educational`
+--
+ALTER TABLE `educational`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -96,10 +152,22 @@ ALTER TABLE `articles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT для таблицы `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `educational`
+--
+ALTER TABLE `educational`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT для таблицы `literature`
 --
 ALTER TABLE `literature`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
