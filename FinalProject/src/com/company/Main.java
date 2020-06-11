a36568d5f9a2bbb6625c4ef7199649a4a25e686d
marara02 <57908460+marara02@users.noreply.github.com>
2020-06-11 17:27:40 +0600

    With buttons but keyListener does not added yet

--- FinalProject/src/com/company/Main.java
+++ FinalProject/src/com/company/Main.java
@@ -1,18 +1,29 @@
 package com.company;
 import com.company.Books.LiteratureBooks;
 import com.company.Books.TypesOfBooks;
-
 import java.sql.SQLException;
 import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        LiteratureBooks literatureBooks = new LiteratureBooks();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter name,genre,cost");
-        String name = scanner.nextLine();
-        int cost =scanner.nextInt();
-        TypesOfBooks typesOfBooks;
-        literatureBooks.setLiterature(cost,name,TypesOfBooks.valueOf("History"));
-        literatureBooks.CheckForCost();
+    Main(){
+        JFrame f=new JFrame("Button");
+        JButton b=new JButton("User");
+        b.setBounds(50,100,100, 40);
+        b.setBackground(java.awt.Color.cyan);
+        b.setOpaque(true);
+        JButton b2=new JButton("Worker");
+        b2.setBounds(160,100,100, 40);
+        b2.setBackground(java.awt.Color.PINK);
+        b2.setOpaque(true);
+        f.add(b);
+        f.add(b2);
+        f.setSize(300,400);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+    public static void main(String[] args) {
+      new Main();
 }
