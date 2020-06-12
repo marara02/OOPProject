0ce35aaf9e8922cf1582262099ee657746937551
ZhanelKaiyr <53339773+ZhanelKaiyr@users.noreply.github.com>
2020-06-12 17:22:02 +0600

    Update Main.java

--- FinalProject/src/com/company/Workers/Main.java
+++ FinalProject/src/com/company/Workers/Main.java
@@ -3,7 +3,7 @@ package Workers;
 
 public class Main {
     public static void main(String[] args) {
-    	
+    	System.out.println("Here we will give you general information about workers");
 Boss boss=new Boss("Assem", "Assemova");
 System.out.println(boss.toString());
         Manager manager = new Manager("Marara","Nuraly");
@@ -12,20 +12,8 @@ System.out.println(boss.toString());
         System.out.println(seller.toString());
         
         manager.AddBook();
+        manager.AddArticle();
         
-        
-        /*User user = new User();
-        Scanner ss = new Scanner(System.in);
-        String full_name = ss.nextLine();
-        String email = ss.nextLine();
-        user.setAll(full_name,email);
-        user.Information_and_choose();
-       /* LiteratureBooks literatureBooks = new LiteratureBooks();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter name,genre,cost");
-        String name = scanner.nextLine();
-        int cost =scanner.nextInt();
-        TypesOfBooks typesOfBooks;
-        literatureBooks.setLiterature(cost,name,TypesOfBooks.valueOf("History"));
-        literatureBooks.CheckForCost();*/
+ 
     }
+}
