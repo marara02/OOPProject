8e8609ea6cf187d6048d78848009c60a9d30c3da
marara02 <57908460+marara02@users.noreply.github.com>
2020-06-12 22:41:33 +0600

    Buttons with choosing if you are user or manager

--- FinalProject/src/com/company/Main.java
+++ FinalProject/src/com/company/Main.java
@@ -9,7 +9,7 @@ import java.awt.event.*;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main implements ActionListener {
+public class Main {
     Main(){
         JFrame f = new JFrame("Button");
         f.setSize(200,200);
@@ -24,6 +24,7 @@ public class Main implements ActionListener {
         b2.setBounds(160, 100, 100, 40);
         b2.setBackground(java.awt.Color.MAGENTA);
         b2.setOpaque(true);
+        b2.addActionListener(new Manager("Janel","Kaiyrbekova"));
 
         f.add(label);
         f.add(b);
@@ -33,28 +34,7 @@ public class Main implements ActionListener {
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args){
         new Main();
-        //Manager manager = new Manager("jjij","yuuy");
-        //manager.AddBook();
-        /*User user = new User();
-        Scanner ss = new Scanner(System.in);
-        String full_name = ss.nextLine();
-        String email = ss.nextLine();
-        user.setAll(full_name,email);
-        user.Information_and_choose();
-       /* LiteratureBooks literatureBooks = new LiteratureBooks();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter name,genre,cost");
-        String name = scanner.nextLine();
-        int cost =scanner.nextInt();
-        TypesOfBooks typesOfBooks;
-        literatureBooks.setLiterature(cost,name,TypesOfBooks.valueOf("History"));
-        literatureBooks.CheckForCost();*/
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-
     }
 }
