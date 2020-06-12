3314e8c0921f8031c851bc7adc008cdd60a2d848
marara02 <57908460+marara02@users.noreply.github.com>
2020-06-12 22:32:01 +0600

    With Seller fuctions

--- FinalProject/src/com/company/User/User.java
+++ FinalProject/src/com/company/User/User.java
@@ -1,13 +1,12 @@
 package com.company.User;
 import com.company.Books.*;
 import com.company.Article.*;
-import com.company.Worker.*;
+import com.company.Workers.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class User implements ActionListener
-{
+public class User implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
@@ -20,102 +19,92 @@ public class User implements ActionListener
         System.out.println("-----------Soul-Mara=>The newest bookshop in the world");
         System.out.println("What do you want to buy?");
         String type = scanner.nextLine();
-        //Books or Article
-        
-        if(type.equals("book")){
-            System.out.println("For which purposes do you need?"); //Educational or Literature
-           String purpose = scanner.nextLine();
-           if(purpose.equals("Educational")){
-               System.out.print("Enter your grade:");
-               int grade = scanner.nextInt();
-               System.out.print("Enter book name:");
-               String name = scanner.nextLine();
-               EducationalBooks educationalBooks = new EducationalBooks.BuilderBook()
-                       .withGrade(grade)
-                       .withName(name)
-                       .build();
-               educationalBooks.CheckExist();
-               if(educationalBooks.CheckExist()) {
-                   educationalBooks.hasAudioBook();
-                   educationalBooks.OnlineBooks();
-                   System.out.println("Are you sure to buy?");
-                   String ans = scanner.nextLine();
-                   if(ans.equals("yes")){
-                            Seller seller = new Seller("Aya", "Shalkar", 12);
-                            seller.SaleBook(name);
+        if (type.equals("book")) {
+            System.out.println("For which purposes do you need?");
+            String purpose = scanner.nextLine();
+            if (purpose.equals("Educational")) {
+                System.out.print("Enter your grade:");
+                int grade = scanner.nextInt();
+                System.out.print("Enter book name:");
+                String name = scanner.nextLine();
+                EducationalBooks educationalBooks = new EducationalBooks.BuilderBook()
+                        .withGrade(grade)
+                        .withName(name)
+                        .build();
+                educationalBooks.CheckExist();
+                if (educationalBooks.CheckExist()) {
+                    educationalBooks.hasAudioBook();
+                    educationalBooks.OnlineBooks();
+                    System.out.println("Are you sure to buy?");
+                    String ans = scanner.nextLine();
+                    if (ans.equals("yes")) {
+                        Seller seller = new Seller("Aya", "Shalkar", 12);
+                        seller.SaleBook(name);
+                    } else {
+                        System.out.println("Continue choose");
+                    }
+                }
+            }
+            if (purpose.equals("Literature")) {
+                System.out.print("Enter name:");
+                String name = scanner.nextLine();
+                LiteratureBooks literatureBooks = new LiteratureBooks.bookBuilder()
+                        .withName(name)
+                        .build1();
+                literatureBooks.CheckExist();
+                if (literatureBooks.CheckExist()) {
+                    System.out.println("Are you sure to buy?");
+                    String answer = scanner.nextLine();
+                    if (answer.equals("yes")) {
+                        Seller seller = new Seller("Aya", "Shalkar", 12);
+                        seller.SaleBook(name);
+                        //literatureBooks.Buy();
+                    } else {
+                        System.out.println("Continue choosing");
+                    }
+                } else {
+                    System.out.println("Does not exist:(");
+                }
+                if (type.equals("Article")) {
+                    System.out.println("What must consist article?");
+                    String genre = scanner.nextLine();
+                    try {
+                        if (genre.equals("Science")) {
+                            System.out.println("name:");
+                            String name1 = scanner.nextLine();
+                            ScientificArticles scientificArticles = new ScientificArticles.Builder()
+                                    .withName(name1)
+                                    .build();
+                            scientificArticles.count_article();
+                            System.out.println("Are you sure to buy?");
+                            String answer = scanner.nextLine();
+                            if (answer.equals("yes")) {
+                                Seller seller = new Seller("Aya", "Shalkar", 12);
+                                seller.SaleArticle(name1);
+                            } else {
+                                System.out.println("Continue to choose");
+                            }
+                        } else {
+                            System.out.println("name:");
+                            String name2 = scanner.nextLine();
+                            FashionArticles fashionArticles = new FashionArticles.BuilderArticle()
+                                    .withName(name2)
+                                    .build();
+                            fashionArticles.count_article();
+                            System.out.println("Are you sure to buy?");
+                            String answer = scanner.nextLine();
+                            if (answer.equals("yes")) {
+                                Seller seller = new Seller("Aya", "Shalkar", 12);
+                                seller.SaleArticle(name);
+                            } else {
+                                System.out.println("Continue to choose");
+                            }
                         }
-                        else{
-                            System.out.println("Continue choose");
-                        }
-               }
-           }
-           
-           if(purpose.equals("Literature")){
-               System.out.print("Enter name:");
-               String name = scanner.nextLine();
-               LiteratureBooks literatureBooks = new LiteratureBooks.bookBuilder()
-                       .withName(name)
-                       .build1();
-               literatureBooks.CheckExist();
-               if(literatureBooks.CheckExist()){
-                 literatureBooks.hasAudioBook();
-                   System.out.println("Are you sure to buy?");
-                   String answer = scanner.nextLine();
-                   if(answer.equals("yes")){
-                     Seller seller = new Seller("Aya", "Shalkar", 12);
-                            seller.SaleBook(name);
-                       //literatureBooks.Buy();
-                   }
-                   else{
-                       System.out.println("Continue choosing");
-                   }
-               }
-           else {
-               System.out.println("Does not exist:(");
-           }
-           
-        if(type.equals("Article")){
-           System.out.println("What must consist article?");
-           String genre = scanner.nextLine();
-           try {
-               if(genre.equals("Science")){
-                   System.out.println("name:");
-                   String name = scanner.nextLine();
-                   ScientificArticles scientificArticles = new ScientificArticles.Builder()
-                           .withName(name)
-                           .build();
-                   scientificArticles.count_article();
-                   System.out.println("Are you sure to buy?");
-                   String answer=scanner.nextLine();
-                   if(answer.equals("yes")){
-                   Seller seller = new Seller("Aya","Shalkar",12);
-                   seller.SaleArticle(name);
-                   }
-                   else{
-                   System.out.println("Continue to choose");
-                   }
-           }
-               else{
-                   System.out.println("name:");
-                   String name2 = scanner.nextLine();
-                   FashionArticles fashionArticles = new FashionArticles.BuilderArticle()
-                           .withName(name2)
-                           .build();
-                   fashionArticles.count_article();
-                   System.out.println("Are you sure to buy?");
-                   String answer=scanner.nextLine();
-                   if(answer.equals("yes")){
-                   Seller seller = new Seller("Aya","Shalkar",12);
-                   seller.SaleArticle(name);
-                   }
-                   else{
-                   System.out.println("Continue to choose");
-                   }
-               }
-           }
-           catch (NullPointerException e){
-               System.err.println(e.getMessage());
-           }
-        }
+                    } catch (NullPointerException e) {
+                        System.err.println(e.getMessage());
+                    }
+                }
+            }
         }
     }
+}
